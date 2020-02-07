5f49c9c7689cb3273c24c8fd652c8832e06a3acf
Jrhoades5 <Jrhoades5@outlook.com>
2020-02-06 21:12:50 -0700

    Not done yet but need a review

---
+++ my_complex.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+struct _complex {
+	double real;
+	double imag;
+};
+typedef struct _complex Complex;
+
+void print_complex(Complex c);
+ 
+
+Complex complex_add(Complex c1, Complex c2);
+Complex complex_sub(Complex c1, Complex c2);
+Complex complex_mult(Complex c1, Complex c2);
+Complex complex_div(Complex c1, Complex c2);
+Complex complex_conj(Complex c);
+
+
+double magnitude(Complex c);
+double phase(Complex c);
+
+
+void hdler_a(Complex (*complex_fn)(Complex, Complex), Complex c1, Complex c2){
+	printf("%lf  %lf\n", complex_fn(c1, c2).real, complex_fn(c1, c2).imag);
+}
+
+
+void hdler_b(double (*complex_fn)(Complex),Complex c1){
+	printf("%lf \n", complex_fn(c1));
+}
+
+
